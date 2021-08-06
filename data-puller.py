cf7e8face37f4e359159e8aa165567e2f718fd57
yarrr-ru <safronov.aa@gmail.com>
2021-08-06 10:05:19 +0300

    Collect more matches

--- data-puller.py
+++ data-puller.py
@@ -12,6 +12,7 @@ class DataPuller(cli.Application):
     API_URL = "https://quake-stats.bethesda.net/api/v2"
     LEADERBOARD_PAGE_SIZE = 100
     MIN_INTERESTING_RATING = 1825
+    ITEM_RESPAWN_COOLDOWN = 30
 
     def make_api_call(self, call, params):
         url = "{}/{}".format(self.API_URL, call)
@@ -20,9 +21,52 @@ class DataPuller(cli.Application):
         assert response.status_code == 200
         return json.loads(response.text)
 
+    def major_items_estimation(self, match_statistics):
+        reports = match_statistics["battleReportPersonalStatistics"]
+        mega = sum(report["megaHealthPickups"] for report in reports)
+        heavy = sum(report["heavyArmorPickups"] for report in reports)
+        return self.ITEM_RESPAWN_COOLDOWN * (max(mega, heavy) - 1)
+
+    def champions_time_estimation(self, match_statistics):
+        reports = match_statistics["battleReportPersonalStatistics"]
+        max_champions_time = 0
+        for report in reports:
+            champions_time = report["championsTime"]
+            assert len(champions_time) == 1
+            for champions, life_time in champions_time.items():
+                max_champions_time = max(life_time, max_champions_time)
+        return max_champions_time // 1000
+
+    def should_estimate_time_limit(self, match_statistics):
+        reports = match_statistics["battleReportPersonalStatistics"]
+        players = [report["nickname"] for report in reports]
+        scores = [report["score"] for report in reports]
+        score_limit = match_statistics["scoreLimit"]
+        return len(players) == 1 or max(scores) == score_limit
+
+    def match_players(self, username, match_statistics):
+        reports = match_statistics["battleReportPersonalStatistics"]
+        players = [report["nickname"] for report in reports]
+        if username not in players:
+            players.append(username)
+        return players
+
+    def calculate_time_limit(self, match_statistics):
+        time_limit = match_statistics["timeLimit"]
+
+        assert self.champions_time_estimation(
+            match_statistics) >= self.major_items_estimation(match_statistics)
+
+        if self.should_estimate_time_limit(match_statistics):
+            return self.champions_time_estimation(match_statistics)
+        else:
+            return time_limit
+
     def main(self):
         for offset in count(0, self.LEADERBOARD_PAGE_SIZE):
-            leaderboard = self.make_api_call("Leaderboard", {"from": offset, "board": "duel", "season": "current"})
+            leaderboard = self.make_api_call(
+                "Leaderboard", {
+                    "from": offset, "board": "duel", "season": "current"})
             assert leaderboard["boardType"] == "duel"
             entries = leaderboard["entries"]
             if len(entries) == 0:
@@ -43,16 +87,13 @@ class DataPuller(cli.Application):
                     assert match_statistics["id"] == match["id"]
                     if match_statistics["gameMode"] != "GameModeClassicDuel":
                         continue
-                    reports = match_statistics["battleReportPersonalStatistics"]
-                    players = [report["nickname"] for report in reports]
+                    players = self.match_players(username, match_statistics)
                     if len(players) != 2:
                         continue
-                    scores = [report["score"] for report in reports]
-                    if max(scores) == match_statistics["scoreLimit"]:
-                        continue
+                    time_limit = self.calculate_time_limit(match_statistics)
                     match_description = {
                         "id": match_statistics["id"],
-                        "time_limit": match_statistics["timeLimit"],
+                        "time_limit": time_limit,
                         "timestamp": match_statistics["playedDateTime"],
                         "players": players
                     }
